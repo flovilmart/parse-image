c05c1b6d0c340f2dad89872c6c072df5f40f061b
Florent Vilmart <vfloz@me.com>
2014-06-18 10:09:12 -0400

    Now installs with js script for windows installations

---
+++ install.js
@@ -0,0 +1,27 @@
+//#!/bin/sh
+//BREW=0;
+//brew --version > /dev/null 2>&1 && BREW=1;
+//if [ $BREW -eq 1 ]
+//then
+//	echo "Installing using brew...";
+//	brew install imagemagick; 
+//	brew install graphicsmagick;
+//else
+//	echo "You should install imagemagick and graphicsmagick"
+//fi
+
+var cmd = 'brew';
+var args = '--version > /dev/null 2>&1 && brew install imagemagick graphicsmagick'
+var exec = require('child_process').exec,
+    child;
+exec(cmd+' '+args,
+  function (error, stdout, stderr) {
+    console.log('' + stdout);
+    if(error !== null){
+        console.warn("Cannot install using brew...\n"+
+                        "Please install imagemagick and "+
+                        "graphicsmagick");
+        console.warn("parse-image will not work properly...");
+        console.warn("\n\nvisit http://www.graphicsmagick.org\n\n");
+    }
+});
