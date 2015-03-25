284f972d380b02c0b5ce6804174d2a858336744a
Florent Vilmart <florentvilmart@me.com>
2015-03-24 23:45:14 -0400

    Fix #1, standalone usage
    Adds support for pad function for full compatibility

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var gm = require('gm')
-
-module.exports = Image = function(data){
-	this.setData(data);
+// Require parse if undefined for the promise
+if (typeof Parse == "undefined") {
+	Parse = require("parse").Parse;
 }
+module.exports = Image = function(){}
 
 Image.prototype.setData = function(data, options){
 	var self = this;
-	var p = new Parse.Promise();
-  	this._image = gm(data,'');
-  	this._image.size(function(err, size){
+  	return self._setData(data, p, options);
+}
+
+Image.prototype._setData = function(data, p, options) {
+	if(typeof p == "undefined") {
+		p = new Parse.Promise()
+	}
+	var self = this;
+	self._data = data;
+	self._image = gm(data);
+  	self._image.size({bufferStream: true}, function(err, size){
   		self._size = size;
   		if (err) {
 			if(options && options.error){
@@ -22,7 +31,7 @@ Image.prototype.setData = function(data, options){
 			p.resolve(self);
 		}
   	})
-	return p;
+  	return p
 }
 
 Image.prototype.width = function(){
@@ -39,8 +48,6 @@ Image.prototype.data = function(){
 
 Image.prototype.crop = function(options){
 	var self = this;
-	var p = new Parse.Promise();
-	console.log(options);
 	var w = options.width;
 	var h = options.height;
 	var l = options.left || 0;
@@ -54,68 +61,76 @@ Image.prototype.crop = function(options){
     if (!options.height) {
     	h = self.height()-b-t;
     }
-    console.log(w+" "+h+" "+l+" ")
-    //self.size = {width: w, height:h}
-	self._wrap(self._image.crop(w,h,l,t), p, options);
-	return p;
+    var cropped = self._image.crop(w,h,l,t);
+	return self._wrap(cropped, options);
 }
 
 Image.prototype._callback = function(p, options){
 	var self = this;
 	var error = function(err){
 		if(options && options.error){
-				options.error(err);
-			}
-			p.reject(err);
+			options.error(err);
+		}
+		p.reject(err);
 	}
 
 	return function(err, buf){
-		self._data = buf;
 		if (err) {
 			error(err);
 		}else{
-			self._image = gm(buf);
-			self._image.size(function(err, size){
-				if (err) {
-					error(err);
-				}else{
-					self._size = size;
-					if(options && options.success){
-						options.success(self);
-					}
-					p.resolve(self);
-				}
-			});
+			self._setData(buf, p, options);
 		}
 	}
 }
 
-Image.prototype._wrap = function(gm,p,options){
+Image.prototype._wrap = function(gm, options){
 	var self = this;
+	var p = new Parse.Promise();
 	gm.toBuffer(self._callback(p,options));
+	return p;
 }
 
 Image.prototype.scale = function(options){
 	var self = this;
-	var p = new Parse.Promise();
-	var p1 = Parse.Promise.as();
 	if(options.ratio){
 		options.width = options.ratio*self.width();
 		options.height = options.ratio*self.height();
 	}
-	//self._size = {width: options.width, height: options.height}
-	self._wrap(self._image.scale(options.width, options.height),p,options);
-	return p;
+	return self._wrap(self._image.scale(options.width, options.height),options);
 }
 
 Image.prototype.setFormat = function(format,options){
 	var self = this;
-	var p = new Parse.Promise();
 	self._image.setFormat(format.toLowerCase());
-	self._wrap(self._image,p,options);
-	return p;
+	return self._wrap(self._image, options);
 }
 
 Image.prototype.format = function(){
 	return this._image.format();
+}
+
+Image.prototype.pad = function(options) {
+	var self = this;
+	var w = options.width;
+	var h = options.height;
+	var l = options.left || 0;
+  	var t = options.top || 0;
+
+  	var r = options.right || 0;
+  	var b = options.bottom || 0;
+
+    if (!options.width) {
+    	w = self.width()+r+l;
+    }
+    if (!options.height) {
+    	h = self.height()+b+t;
+    }
+
+    var padded = self._image.out("-background", options.color)
+    	.borderColor(options.color)
+    	.border(l, r)
+    	.extent(w, h)
+    	.out("-flatten")
+
+    return self._wrap(padded, options);
 }
